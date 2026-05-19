b6a4ec625a5555a0356ec9b81006d57af859125c
Danilo Teixeira Rufino da SIlva <danilotx29@gmail.com>
2026-05-19 11:16:25 -0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function calcularIdade(){
-    const anoDigitado = document.getElementById('inputAno').value;
-
-    const anoAtual = new Date().getFullYear();
-
-    const idade = anoAtual - anoDigitado;
-    document.getElementById('resultado').innerHTML = `A sua idade é: ${idade} anos.`;
-}
